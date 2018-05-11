08aaded0587c3989546419e9087d1d0226b98521
artificerpi <11538418+artificerpi@users.noreply.github.com>
2018-05-11 22:54:55 +0800

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import './rxjs-extensions';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
+import { HeroService } from './hero.service';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/hero-search.component.ts
@@ -0,0 +1,46 @@
+import { Component,OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+
+import { HeroSearchService } from './hero-search.service';
+import { Hero } from './hero'; 
+
+@Component({
+    // moduleId: module.id,
+    selector: 'hero-search',
+    templateUrl: 'app/hero-search.component.html',
+    styleUrls: ['app/hero-search.component.css'],
+    providers: [HeroSearchService]
+})
+
+export class HeroSearchComponent implements OnInit{
+    heroes: Observable<Hero[]>;
+    private searchTerms = new Subject<string>();
+
+    constructor(
+        private heroSearchService: HeroSearchService,
+        private router: Router){}
+
+    // Push a search term into the Observable stream
+    search(term: string): void{
+        this.searchTerms.next(term);
+    }
+    ngOnInit(): void{
+        this.heroes = this.searchTerms
+            .debounceTime(3000)
+            .distinctUntilChanged()
+            .switchMap(term => term
+                ?this.heroSearchService.search(term)
+                :Observable.of<Hero[]>([]))
+                .catch(error => {
+                    console.log(error);
+                    return Observable.of<Hero[]>([]);
+                });
+    }
+
+    gotoDetail(hero: Hero): void{
+        let link = ['/detail', hero.id];
+        this.router.navigate(link);
+    }
+}
