a9af048c836ca6335c8a5059dd09b67c318c1249
artificerpi <artificerpi@outlook.com>
2019-05-20 23:15:53 +0800

    Add pwa support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { CoreModule } from './core/core.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
     BrowserModule,
     CoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   declarations: [
     AppComponent
