6d503624c63199acad8eab2efecaa86a3e2e99d1
artificerpi <11538418+artificerpi@users.noreply.github.com>
2020-06-13 23:56:06 +0800

    Fix i18n issues

--- src/app/shared/in-memory-data.service.ts
+++ src/app/shared/in-memory-data.service.ts
@@ -12,8 +12,8 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 17, name: 'Dynama' },
       { id: 18, name: 'Dr IQ' },
       { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' },
-      { id: 21 }
+      { id: 20, name: 'Tornado' }
+      // { id: 21 } // no name
     ];
     return { heroes };
   }
