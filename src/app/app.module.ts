1389249672a5afca3493040a1edbdf72e6ff7ed6
qtopie.rw <qtopie.rw@gmail.com>
2022-12-28 23:27:34 +0800

    \organize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
-import {CoreModule} from './core/core.module';
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {environment} from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   imports: [
